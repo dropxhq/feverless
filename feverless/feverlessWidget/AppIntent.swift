0254fc56538a577d87bf5e70b3382d00153cbec1
Max Lin <jason.max.lin@outlook.com>
2026-05-20 08:40:09 +0800

    feat: add widget

---
+++ feverless/feverlessWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  feverlessWidget
+//
+//  Created by Monster 林 on 2026/5/20.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ feverless/feverlessWidget/feverlessWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  feverlessWidgetBundle.swift
+//  feverlessWidget
+//
+//  Created by Monster 林 on 2026/5/20.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct feverlessWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        feverlessWidget()
+    }
+}
